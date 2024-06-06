39af9e54ac8d6baec1522f9fc6284282d05b3f9a
Niccolò Cantù <niccolo.cantu@nina.no>
2024-06-06 12:19:04 +0200

    add back unfold

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -70,8 +70,12 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # "django.contrib.humanize", # Handy template tags
-    "django.contrib.admin",
     "django.forms",
+]
+
+ADMIN_APPS = [
+    "unfold",
+    "django.contrib.admin",
 ]
 
 THIRD_PARTY_APPS = [
@@ -108,7 +112,7 @@ LOCAL_APPS = [
     "theme",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = DJANGO_APPS + ADMIN_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 # MIGRATIONS
 # ------------------------------------------------------------------------------

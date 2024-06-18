2653800e86ee9f64422466093138d4df9a385009
Niccolò Cantù <niccolo.cantu@nina.no>
2024-06-18 14:03:03 +0200

    improve field rendering

--- src/apps/core/templatetags/core.py
+++ src/apps/core/templatetags/core.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import fields as djfields
+from taggit.managers import TaggableManager
 
 register = template.Library()
 
@@ -8,10 +10,30 @@ def verbose_name(instance):
     return instance._meta.verbose_name
 
 
+def render(field, instance):
+    v = getattr(instance, field.name)
+
+    if isinstance(field, djfields.related.ManyToManyField):
+        return field.verbose_name or field.name, ",".join([str(e) for e in v.all()])
+
+    if isinstance(field, djfields.related.ManyToOneRel):
+        return None, None
+
+    return field.verbose_name or field.name, str(v)
+
+
+IGNORED_FIELDS = ["tagged_items"]
+
+
 @register.filter
 def get_fields(instance, fields=None):
-    return (
-        (field, field.value_to_string(instance))
-        for field in instance._meta.fields
-        if not fields or field.name in fields.split(" ")
+    return filter(
+        lambda x: x[0],
+        (
+            render(field, instance)
+            for field in instance._meta.get_fields()
+            if (not fields or field.name in fields.split(" "))
+            and not isinstance(field, TaggableManager)
+            and field.name not in IGNORED_FIELDS
+        ),
     )

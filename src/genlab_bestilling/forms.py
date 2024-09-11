2c4aab9183b7b8acfe8de91dc8ae491dae7e123a
Niccolò Cantù <niccolo.cantu@nina.no>
2024-09-11 09:49:19 +0200

    include date range picker

--- src/genlab_bestilling/forms.py
+++ src/genlab_bestilling/forms.py
@@ -6,6 +6,7 @@ from django import forms
 # from django.core.exceptions import ValidationError
 from django.forms.renderers import BaseRenderer
 from django.forms.utils import ErrorList
+from formset.ranges import DateRangePicker
 from formset.renderers.tailwind import FormRenderer
 from formset.utils import FormMixin
 from formset.widgets import DualSortableSelector, Selectize
@@ -54,7 +55,7 @@ class GenrequestForm(FormMixin, forms.ModelForm):
             "sample_types",
             "analysis_types",
             "expected_total_samples",
-            # "analysis_timerange",
+            "analysis_timerange",
         )
         widgets = {
             "area": Selectize(search_lookup="name_icontains"),
@@ -67,6 +68,7 @@ class GenrequestForm(FormMixin, forms.ModelForm):
             ),
             "sample_types": DualSortableSelector(search_lookup="name_icontains"),
             "analysis_types": DualSortableSelector(search_lookup="name_icontains"),
+            "analysis_timerange": DateRangePicker(),
         }
 
 
@@ -78,6 +80,7 @@ class GenrequestEditForm(GenrequestForm):
             "sample_types",
             "analysis_types",
             "expected_total_samples",
+            "analysis_timerange",
         )
 
     # def clean_species(self) -> dict[str, Any]:

1875c1efa720bf4c6fe9d13d6ed1ef822d93fb1b
Niccolò Cantù <niccolo.cantu@nina.no>
2024-12-03 15:33:09 +0100

    generate genlab ids

--- src/genlab_bestilling/tasks.py
+++ src/genlab_bestilling/tasks.py
@@ -1,6 +1,8 @@
 from procrastinate.contrib.django import app
 
+from .libs.genlabid import generate as generate_genlab_id
 
-@app.task(name="print-hello")
-def print_hello():
-    print("hello")
+
+@app.task(name="generate-genlab-ids")
+def generate_ids(order_id):
+    generate_genlab_id(order_id)

ba779099f420c43fb620db9062623dfa6d689e68
Niccolò Cantù <niccolo.cantu@nina.no>
2024-12-13 13:38:27 +0100

    disable isolation

--- src/genlab_bestilling/tasks.py
+++ src/genlab_bestilling/tasks.py
@@ -1,10 +1,11 @@
 from procrastinate.contrib.django import app
 
 from .libs.genlabid import generate as generate_genlab_id
-from .libs.isolation import isolate
+
+# from .libs.isolation import isolate
 
 
 @app.task(name="generate-genlab-ids")
 def generate_ids(order_id):
     generate_genlab_id(order_id=order_id)
-    isolate(order_id=order_id)
+    # isolate(order_id=order_id)

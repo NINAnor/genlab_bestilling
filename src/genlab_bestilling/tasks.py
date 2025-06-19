0ad91770de68fbcc0f8af0b2cc40c5d16278e4a0
Francesco Frassinelli <francesco.frassinelli@nina.no>
2025-06-19 09:48:06 +0000

    Auto retry generate_ids on OperationalError (#72)

--- src/genlab_bestilling/tasks.py
+++ src/genlab_bestilling/tasks.py
@@ -1,11 +1,17 @@
+# from .libs.isolation import isolate
+from django.db.utils import OperationalError
+from procrastinate import RetryStrategy
 from procrastinate.contrib.django import app
 
 from .libs.genlabid import generate as generate_genlab_id
 
-# from .libs.isolation import isolate
 
-
-@app.task(name="generate-genlab-ids")
+@app.task(
+    name="generate-genlab-ids",
+    retry=RetryStrategy(
+        max_attempts=5, linear_wait=5, retry_exceptions={OperationalError}
+    ),
+)
 def generate_ids(order_id):
     generate_genlab_id(order_id=order_id)
     # isolate(order_id=order_id)

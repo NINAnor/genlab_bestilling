cf37435c135475c0fbaf9622cb76b35125540436
Morten Madsen Lyngstad <morten.lyngstad@bekk.no>
2025-06-25 09:56:53 +0200

    Remove unnecessary whitespace in DashboardView context preparation

--- src/staff/views.py
+++ src/staff/views.py
@@ -65,7 +65,7 @@ class DashboardView(StaffMixin, TemplateView):
 
         urgent_orders = Order.objects.filter(is_urgent=True)
         context["urgent_orders"] = urgent_orders
-        
+
         confirmed_orders = Order.objects.filter(status=Order.OrderStatus.DELIVERED)
         context["confirmed_orders"] = confirmed_orders
         context["now"] = now()

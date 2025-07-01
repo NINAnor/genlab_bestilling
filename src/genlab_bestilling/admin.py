71073c1c7b01487b66452477ed2e6dacb0800851
Emil Telstad <22004178+emilte@users.noreply.github.com>
2025-07-01 13:48:24 +0200

    Improve EquimentOrderQuantityAdmin.

--- src/genlab_bestilling/admin.py
+++ src/genlab_bestilling/admin.py
@@ -155,9 +155,18 @@ class AnalysisTypeAdmin(ModelAdmin):
 
 @admin.register(EquipmentType)
 class EquipmentTypeAdmin(ModelAdmin):
-    list_display = ["name", "unit"]
-    list_filter = ["unit"]
-    search_fields = ["name"]
+    M = EquipmentType
+    list_display = [M.name.field.name, M.unit.field.name]
+
+    search_help_text = "Search for equipment type name"
+    search_fields = [M.name.field.name]
+
+    list_filter = [
+        (M.name.field.name, unfold_filters.FieldTextFilter),
+        (M.unit.field.name, unfold_filters.FieldTextFilter),
+    ]
+    list_filter_submit = True
+    list_filter_sheet = False
 
 
 @admin.register(EquipmentBuffer)
@@ -168,7 +177,35 @@ class EquipmentBufferAdmin(ModelAdmin):
 
 
 @admin.register(EquimentOrderQuantity)
-class EquimentOrderQuantityAdmin(ModelAdmin): ...
+class EquimentOrderQuantityAdmin(ModelAdmin):
+    M = EquimentOrderQuantity
+    list_display = [
+        M.id.field.name,
+        M.equipment.field.name,
+        M.order.field.name,
+        M.buffer.field.name,
+        M.buffer_quantity.field.name,
+        M.quantity.field.name,
+    ]
+    search_help_text = "Search for id"
+    search_fields = [
+        M.id.field.name,
+    ]
+    list_filter = [
+        (M.id.field.name, unfold_filters.SingleNumericFilter),
+        (M.equipment.field.name, unfold_filters.AutocompleteSelectMultipleFilter),
+        (M.order.field.name, unfold_filters.AutocompleteSelectMultipleFilter),
+        (M.buffer.field.name, unfold_filters.AutocompleteSelectMultipleFilter),
+        (M.buffer_quantity.field.name, unfold_filters.RangeNumericFilter),
+        (M.quantity.field.name, unfold_filters.RangeNumericFilter),
+    ]
+    autocomplete_fields = [
+        M.equipment.field.name,
+        M.order.field.name,
+        M.buffer.field.name,
+    ]
+    list_filter_sheet = False
+    list_filter_submit = True
 
 
 @admin.register(EquipmentOrder)

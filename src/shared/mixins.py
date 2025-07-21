ca3711db1c73865889e74956afcecd050941c092
Emil Telstad <22004178+emilte@users.noreply.github.com>
2025-07-21 14:33:46 +0200

    Update docstring.

--- src/shared/mixins.py
+++ src/shared/mixins.py
@@ -1,11 +1,16 @@
 class CleanSaveMixin:
     """
     Mixin to ensure that the save method is called with object validation.
+    Intended for classes that inherit from Django Model.
     https://docs.djangoproject.com/en/5.2/ref/models/instances/#validating-objects
 
+    NOTE:
     To validate instances, implement the `clean` method.
     It's a hook designed to be overridden by subclasses to perform custom
     validation logic across multiple fields.
+
+    (For historical reasons, the `clean` method is not called automatically by Django
+    when saving an object. Instead, it must be called explicitly in the `save` method.)
 
     Example:
     ```
@@ -14,7 +19,6 @@ class CleanSaveMixin:
     def clean(self) -> None:
         if self.is_candy and 'candy' not in self.name:
             raise ValidationError("Name must contain 'candy' if object is marked as candy.")
-        pass
     ```
     """  # noqa: E501
 

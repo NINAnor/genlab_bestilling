fe304baac486f0ac7f867ec066f36516f3595fc7
Emil Telstad <22004178+emilte@users.noreply.github.com>
2025-07-22 17:02:36 +0200

    Improve ProjectAdmin. (#185)

--- src/nina/admin.py
+++ src/nina/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
+from unfold.contrib.filters import admin as unfold_filters
 
 from .models import (
     Project,
@@ -14,10 +15,26 @@ class ProjectMembershipInline(admin.TabularInline):
 
 @admin.register(Project)
 class ProjectAdmin(ModelAdmin):
-    search_fields = ["number", "name"]
-    list_filter = ["active"]
-    list_display = ["number", "name", "active"]
+    M = Project
+    list_display = [
+        M.number.field.name,
+        M.name.field.name,
+        M.active.field.name,
+        M.verified_at.field.name,
+    ]
 
+    search_help_text = "Search by project number or name"
+    search_fields = [M.number.field.name, M.name.field.name]
+
+    readonly_fields = [M.verified_at.field.name]
+    list_filter = [
+        (M.number.field.name, unfold_filters.FieldTextFilter),
+        (M.name.field.name, unfold_filters.FieldTextFilter),
+        M.active.field.name,
+        M.verified_at.field.name,
+    ]
+    list_filter_submit = True
+    list_filter_sheet = False
     inlines = [ProjectMembershipInline]
 
 

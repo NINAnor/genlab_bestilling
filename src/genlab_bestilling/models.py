57aad3aa996e80acaadc527f089aa8d6ea676214
Bertine <112892518+aastabk@users.noreply.github.com>
2025-07-23 10:32:56 +0200

    Show new fields analysis samples 2 (#367)
    * Added PCR, analysis and output field to samples
    
    * Added PCR, analysis and output for analysis samples.
    
    * Fix linter error
    
    * Changed comment

--- src/genlab_bestilling/models.py
+++ src/genlab_bestilling/models.py
@@ -614,6 +614,11 @@ class SampleMarkerAnalysis(models.Model):
     marker = models.ForeignKey(f"{an}.Marker", on_delete=models.PROTECT)
     transaction = models.UUIDField(blank=True, null=True)
 
+    # Fields for status tracking
+    has_pcr = models.BooleanField(default=False)
+    is_analysed = models.BooleanField(default=False)
+    is_outputted = models.BooleanField(default=False)
+
     objects = managers.SampleAnalysisMarkerQuerySet.as_manager()
 
     class Meta:

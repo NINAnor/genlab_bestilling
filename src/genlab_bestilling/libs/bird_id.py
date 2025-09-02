66ac6c192caf50bac08b6e4ffe739abfed42bcbb
Niccolo Cantu <niccolo.cantu@nina.no>
2025-09-02 16:15:03 +0200

    fix: mypy

--- src/genlab_bestilling/libs/bird_id.py
+++ src/genlab_bestilling/libs/bird_id.py
@@ -8,7 +8,9 @@ def bird_id(genlab_id: str | None) -> str | None:
     exp = re.compile(r"^G\d{2}([A-Z]{1,3})0*(\d+)(-\d+)?")
 
     match = exp.match(genlab_id)
-    species_code = match.group(1)
-    running_number = match.group(2)
-    replica = match.group(3) if match.group(3) else ""
-    return species_code + running_number + replica
+    if match:
+        species_code = match.group(1)
+        running_number = match.group(2)
+        replica = match.group(3) if match.group(3) else ""
+        return species_code + running_number + replica
+    return None

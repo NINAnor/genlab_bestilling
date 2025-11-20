76b092058e178e4932ac584dd2e6e366b5fa6094
Niccolo Cantu <niccolo.cantu@nina.no>
2025-11-21 00:45:39 +0100

    fix: downloadable files from s3

--- src/config/storages.py
+++ src/config/storages.py
@@ -5,3 +5,4 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = settings.MEDIA_BASE_LOCATION
     file_overwrite = True
+    querystring_auth = True
